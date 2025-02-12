15344e3cf43ccd22a92215d36031380d45a9555e
Banlone <24185079+Banlone@users.noreply.github.com>
2025-02-12 15:00:24 +0100

    .

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739367945',resources: [
+    cacheName: 'chirpy-1739368802',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
